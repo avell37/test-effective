91a41c062c26cc1a1c6eaf0dbfa9982fed37ea92
avell37 <shrokov.37@gmail.com>
2026-05-07 16:08:15 +0300

    feat: added login method, validation and interface

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,17 +1,31 @@
-import { Request } from "express";
-import { registerSchema } from "../validators/auth.validator";
+import { Request, Response } from "express";
+import { loginSchema, registerSchema } from "../validators/auth.validator";
 import { AuthService } from "../services/auth.service";
 
 export class AuthController {
-    static async register(req: Request): Promise<boolean> {
+    static async register(req: Request, res: Response): Promise<Response> {
         try {
             const validatedData = registerSchema.parse(req.body);
 
             await AuthService.register(validatedData);
 
-            return true;
+            return res
+                .status(201)
+                .json({ message: "Пользователь успешно зарегистрирован" });
         } catch (err) {
             throw new Error(`Ошибка при регистрации пользователя: ${err}`);
+        }
+    }
+
+    static async login(req: Request, res: Response): Promise<Response> {
+        try {
+            const validatedData = loginSchema.parse(req.body);
+
+            const token = await AuthService.login(validatedData);
+
+            return res.status(200).json({ token });
+        } catch (err) {
+            throw new Error(`Ошибка при входе пользователя: ${err}`);
         }
     }
 }
--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,3 +1,8 @@
+export interface LoginData {
+    email: string;
+    password: string;
+}
+
 export interface RegisterData {
     fullName: string;
     birthDate: string;
